bd8b27f414da8385b60ce4ac548228766045380c
isaiahakello <isaiahakello19@gmail.com>
2017-04-07 16:58:04 +0300

    fixed some minor bugs

--- complete_incomplete.py
+++ complete_incomplete.py
@@ -1,25 +1,22 @@
 def complete(learning_outcomes):
     complete = {}
-
     for j in learning_outcomes:
         # print j
         for k in learning_outcomes[j]:
             # print k
             for i in learning_outcomes[j][k]:
                 # print i,
-                if i == 'N':
-                    # print i
-                    # incomplete = {k:v for x, v in k if k[i] == 'Not done'}
-                    incomplete[k] = 'Not done'
-                elif i == 'Y':
+                if i == 'Y':
                     # complete = {k:v for x, v in k if k[i] == 'done'}
                     complete[k] = 'done'
+                else:
+                    pass
 
     # return "\n".join(incomplete.keys())
     return "\n".join(complete.keys())
 
 def incomplete(learning_outcomes):
-    complete = {}
+    incomplete = {}
 
     for j in learning_outcomes:
         # print j
@@ -31,9 +28,11 @@ def incomplete(learning_outcomes):
                     # print i
                     # incomplete = {k:v for x, v in k if k[i] == 'Not done'}
                     incomplete[k] = 'Not done'
-                elif i == 'Y':
-                    # complete = {k:v for x, v in k if k[i] == 'done'}
-                    complete[k] = 'done'
+                else:
+                    pass
 
     return "\n".join(incomplete.keys())
     # return "\n".join(complete.keys())
+
+
+print incomplete(learning_outcomes)
